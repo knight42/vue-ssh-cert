af4d4716ae9217efa43f9a756f07c56ddb58605f
knight42 <anonymousknight96@gmail.com>
2017-06-03 01:39:39 +0800

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './ui',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: require.resolve('jquery'),
+        use: [
+          { loader: 'expose-loader', options: 'jQuery' },
+          { loader: 'expose-loader', options: '$' }
+        ]
+      },
+
+      {
+        test: require.resolve('tether'),
+        use: [
+          { loader: 'expose-loader', options: 'Tether' }
+        ]
+      },
+
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      "/api": "http://localhost:3000/"
+    },
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  plugins: [],
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
